1c4d30a925bf4dd24c0d683e376c750f849dc9f0
ndunguuu <bndungu061@gmail.com>
2025-08-18 12:36:53 +0300

    few changes

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
+import BookingList from '../components/BookingList';
 
 const AdminDashboard = () => (
   <div>
     <h2>Mechanic Admin Dashboard</h2>
-    <p style={{ fontSize: '1.2rem', marginTop: 24 }}><strong>No service requests at the moment.</strong></p>
-    <p style={{ marginTop: 12 }}>Mechanics will see service requests and offer remote advice here.</p>
+    <BookingList />
   </div>
 );
 
-export default AdminDashboard; 
+export default AdminDashboard;
